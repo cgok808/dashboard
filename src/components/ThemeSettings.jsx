fb659224710b85c43cd2f76028ef6b27a4b1dab3
Chay <chaylin808@gmail.com>
2022-06-27 05:04:59 +0900

    line+area charts

--- src/components/ThemeSettings.jsx
+++ src/components/ThemeSettings.jsx
@@ -12,9 +12,9 @@ const ThemeSettings = () => {
 
   return (
     <div className='bg-half-transparent w-screen fixed nav-item top-0 right-0'>
-      <div className='float-right h-screen dark:text-gray-200 bg-white dark:[#484B52] w-400'>
+      <div className='float-right h-screen dark:text-gray-200  bg-white dark:bg-[#484B52] w-400'>
         <div className='flex justify-between items-center p-4 ml-4'>
-          <p className='font-semibold text-xl'>Settings</p>
+          <p className='font-semibold text-lg'>Settings</p>
           <button
             type='button'
             onClick={() => setThemeSettings(false)}
@@ -25,7 +25,8 @@ const ThemeSettings = () => {
           </button>
         </div>
         <div className='flex-col border-t-1 border-color p-4 ml-4'>
-          <p className='font-semibold text-lg'>Theme Options</p>
+          <p className='font-semibold text-xl '>Theme Option</p>
+
           <div className='mt-4'>
             <input
               type='radio'
@@ -40,14 +41,14 @@ const ThemeSettings = () => {
               Light
             </label>
           </div>
-          <div className='mt-4'>
+          <div className='mt-2'>
             <input
               type='radio'
               id='dark'
               name='theme'
               value='Dark'
-              className='cursor-pointer'
               onChange={setMode}
+              className='cursor-pointer'
               checked={currentMode === "Dark"}
             />
             <label htmlFor='dark' className='ml-2 text-md cursor-pointer'>
@@ -55,8 +56,8 @@ const ThemeSettings = () => {
             </label>
           </div>
         </div>
-        <div className='flex-col border-t-1 border-color p-4 ml-4'>
-          <p className='font-semibold text-lg'>Theme Colors</p>
+        <div className='p-4 border-t-1 border-color ml-4'>
+          <p className='font-semibold text-xl '>Theme Colors</p>
           <div className='flex gap-3'>
             {themeColors.map((item, index) => (
               <TooltipComponent
@@ -64,7 +65,10 @@ const ThemeSettings = () => {
                 content={item.name}
                 position='TopCenter'
               >
-                <div className='relative mt-2 cursor-pointer flex gap-5 items-center'>
+                <div
+                  className='relative mt-2 cursor-pointer flex gap-5 items-center'
+                  key={item.name}
+                >
                   <button
                     type='button'
                     className='h-10 w-10 rounded-full cursor-pointer'
@@ -74,7 +78,6 @@ const ThemeSettings = () => {
                     <BsCheck
                       className={`ml-2 text-2xl text-white ${
                         item.color === currentColor ? "block" : "hidden"
-                      }
                       }`}
                     />
                   </button>
